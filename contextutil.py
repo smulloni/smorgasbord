3b7c0bfb8fc49758601c323c0d4e7cb9bfd8d547
jsmullyan <jsmullyan@localhost>
2009-05-18 17:02:45 +0000

    version bump to 0.3; fix for nested context objects.

--- contextutil.py
+++ contextutil.py
@@ -1,7 +1,12 @@
+from django.template.context import Context
 
 def context_to_dict(ctxt):
     res={}
     for d in reversed(ctxt.dicts):
-        res.update(d)
+        # sometimes contexts will be nested -- oy!
+        if isinstance(d, Context):
+            res.update(context_to_dict(d))
+        else:
+            res.update(d)
     return res
 
